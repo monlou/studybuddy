d17efba782cb2f2edbed2f8f96c3113f6ec39317
nsv <volsci@tuta.io>
2018-10-11 19:15:11 +1000

    Bind ChatBox Text to ViewModel and Print to Console

--- StudyBuddy/StudyBuddy/ViewModels/ChatPageViewModel.cs
+++ StudyBuddy/StudyBuddy/ViewModels/ChatPageViewModel.cs
@@ -9,6 +9,8 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 
+#pragma warning disable CS0114 // Member hides inherited member; missing override keyword
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
 namespace StudyBuddy.ViewModels
 {
 	public class ChatPageViewModel : BindableBase 
@@ -16,17 +18,18 @@ namespace StudyBuddy.ViewModels
 
         //private INavigationService _navigationService;
         //public EventHandler EditorCompleted { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public System.Windows.Input.ICommand EditorCompletedCommand { get; protected set; }
 
-
-        private string _message = null;
+        private string _message;
         public string Message
         {
             get { return _message; }
             set
             {
                 _message = value;
-                RaisePropertyChanged(nameof(Message));
+                OnPropertyChanged(nameof(Message));
             }
         }
 
@@ -37,11 +40,20 @@ namespace StudyBuddy.ViewModels
 
         }
 
-        private void SendEditorMessage(object param)
+        public void SendEditorMessage(object param)
         {
             Console.WriteLine("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-            Console.WriteLine(param);
+            Console.WriteLine(Message);
 
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+#pragma warning restore CS0114 // Member hides inherited member; missing override keyword
