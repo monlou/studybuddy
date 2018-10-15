db58177b8f72a6c94b2e765860706bacb54c3762
Monique <moniquewinkjames@outlook.com>
2018-10-15 17:01:01 +1000

    ViewModel setup

--- StudyBuddy/StudyBuddy/ViewModels/FlashCardsPageViewModel.cs
+++ StudyBuddy/StudyBuddy/ViewModels/FlashCardsPageViewModel.cs
@@ -1,18 +1,56 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using StudyBuddy.Services.Contracts;
+using Prism.Services;
+using StudyBuddy.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 
 namespace StudyBuddy.ViewModels
 {
 	public class FlashCardsPageViewModel : BindableBase
 	{
-        public System.Windows.Input.ICommand EditorFABCommand { get; protected set; }
+        public event PropertyChangedEventHandler ChangedProperty;
+        public System.Windows.Input.ICommand SaveCardCommand { get; protected set; }
+        private string _input;
+        public string Input
+        {
+            get { return _input; }
+            set
+            {
+                _input = value;
+                OnChangedProperty(nameof(Input));
+            }
+        }
 
         public FlashCardsPageViewModel()
         {
-
+            SaveCardCommand = new Command(SaveCard);
         }
-	}
+
+        public void SaveCard()
+        {
+            Card flashcard = new Card()
+            {
+                CreatorID = 0,
+                CreatorName = "Test",
+                QuestionText = Input,//"Powerhouse of the cell",
+                CorrectText = "Mitochondria",
+                WrongTextOne = "Nucleus",
+                WrongTextTwo = "Chromatin"
+            };
+            Input = "";
+            Console.WriteLine("Input is now: " + Input);
+        }
+
+        private void OnChangedProperty([CallerMemberName] string propertyName = "")
+        {
+            var handler = ChangedProperty;
+            if (handler != null)
+                ChangedProperty(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
