1cd76370aabe8f770118211271d51ca9134f22fc
Monique <moniquewinkjames@outlook.com>
2018-10-19 13:23:22 +1000

    Add Google Button

--- StudyBuddy/StudyBuddy/obj/Debug/netstandard2.0/Views/MainPage.xaml.g.cs
+++ StudyBuddy/StudyBuddy/obj/Debug/netstandard2.0/Views/MainPage.xaml.g.cs
@@ -13,12 +13,16 @@
 namespace StudyBuddy.Views {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views\\MainPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views/MainPage.xaml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.TapGestureRecognizer GoogleButton;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            GoogleButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.TapGestureRecognizer>(this, "GoogleButton");
         }
     }
 }
