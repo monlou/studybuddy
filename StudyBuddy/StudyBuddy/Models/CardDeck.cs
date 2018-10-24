dec7bf03af3b4e5379e44326cd003b46772f1bbe
nsv <volsci@tuta.io>
2018-10-24 21:58:00 +1000

    Add Button Click to Group Frame

---
+++ StudyBuddy/StudyBuddy/Models/CardDeck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace StudyBuddy.Models
+{
+    public class Card
+    {
+        public string ObjType { get; set; }
+        public int CreatorID { get; set; }
+        public string CreatorName { get; set; }
+        public string QuestionText { get; set; }
+        public string CorrectText { get; set; }
+        public string WrongTextOne { get; set; }
+        public string WrongTextTwo { get; set; }
+    }
+}
