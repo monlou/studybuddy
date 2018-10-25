9a6b37c8a964c01d56e56d0f460707a8a0693f13
Monique <moniquewinkjames@outlook.com>
2018-10-25 11:32:04 +1000

    Add Button Press Animation to Google Sign In

--- StudyBuddy/StudyBuddy/obj/Debug/netstandard2.0/Views/MainPage.xaml.g.cs
+++ StudyBuddy/StudyBuddy/obj/Debug/netstandard2.0/Views/MainPage.xaml.g.cs
@@ -17,12 +17,8 @@ namespace StudyBuddy.Views {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
-        private global::Xamarin.Forms.TapGestureRecognizer GoogleButton;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            GoogleButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.TapGestureRecognizer>(this, "GoogleButton");
         }
     }
 }
