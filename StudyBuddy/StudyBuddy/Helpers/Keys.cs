04a67d81d7f05597dd5323741e4feb4819803fd7
nsv <volsci@tuta.io>
2018-11-02 21:12:59 +1000

    Remove keys

--- StudyBuddy/StudyBuddy/Helpers/Keys.cs
+++ StudyBuddy/StudyBuddy/Helpers/Keys.cs
@@ -6,6 +6,7 @@ namespace StudyBuddy.Helpers
 {
     class Keys
     {
+        // Add the keys here.
         public const string CosmosDBUri = "https://studybuddydb.documents.azure.com:443/";
         public const string CosmosDBKey = "t3QY109UXVDHD04fDD1MteFmvJlsL0BWHF5iqvJyf8D0WwSXXf0AHav9UXyhLBwgDWraKoX8yS7ewZ8DfX6lXQ==";
     }
